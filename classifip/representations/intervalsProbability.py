a8783e3a63f5aebcee81db166d61ddb165e20b80
Sebastien Destercke <sdestercke@gmail.com>
2015-04-29 13:51:18 +0200

    redefined logical operators for intervalsProbability

--- classifip/representations/intervalsProbability.py
+++ classifip/representations/intervalsProbability.py
@@ -19,9 +19,26 @@ class IntervalsProbability(CredalSet):
     upper bound | 0.500 0.500 0.500`
     lower bound | 0.100 0.100 0.100
     
-    .. todo::
+    >>> ip2=array([[0.4, 0.5, 0.6], [0., 0.1, 0.2]])
+    >>> intprob2=IntervalsProbability(ip2)
+    >>> print intprob & intprob2
+                     y0    y1    y2 
+               --------------------
+    upper bound | 0.400 0.500 0.500
+    lower bound | 0.100 0.100 0.200
+
+    >>> print intprob | intprob2
+                     y0    y1    y2 
+               --------------------
+    upper bound | 0.500 0.500 0.600
+    lower bound | 0.000 0.100 0.100
     
-        * allow user to give list of list as well as np.array
+    >>> print intprob + intprob2
+                     y0    y1    y2 
+               --------------------
+    upper bound | 0.450 0.500 0.550
+    lower bound | 0.050 0.100 0.150
+    
     """
     
     def __init__(self,lproba):
@@ -170,157 +187,38 @@ class IntervalsProbability(CredalSet):
         str3+=str2
         str3+="\n"
         return str3
-        
-class SetIntProba(object):
-    """Class to handle sets of Int Proba
-
-    Argument:
-    intlist -- a mx2xn array containing upper (1st row) and lower (2nd row) bounds
-               of the m probability intervals
-               
-               dim 1: index of probability set
-               dim 2: lower or upper prob bounds
-               dim 3: values of bounds on each element
-               
-    >>> from numpy import array
-    >>> setproba=array([[[0.6,0.5,0.2],[0.4,0.3,0.]],[[0.55,0.55,0.2],[0.35,0.35,0.]],
-    ... [[0.5,0.2,0.6],[0.3,0.,0.4]],[[0.35,0.6,0.35],[0.15,0.4,0.15]]])
-    >>> from classifip.representations.intervalsProbability import SetIntProba
-    >>> setip=SetIntProba(setproba)
-    >>> setip.intlist
-    array([[[ 0.6 ,  0.5 ,  0.2 ],
-            [ 0.4 ,  0.3 ,  0.  ]],
     
-           [[ 0.55,  0.55,  0.2 ],
-            [ 0.35,  0.35,  0.  ]],
-    
-           [[ 0.5 ,  0.2 ,  0.6 ],
-            [ 0.3 ,  0.  ,  0.4 ]],
-    
-           [[ 0.35,  0.6 ,  0.35],
-            [ 0.15,  0.4 ,  0.15]]])
-            
-    """
-    
-    def __init__(self,intlist):
-        if intlist.__class__.__name__ != 'ndarray':
-            raise Exception('Expecting a numpy array as argument')
-        if intlist.ndim != 3:
-            raise Exception('Expecting a 3-dimensional array')
-        self.intlist=intlist
-        self.nbProbInt=intlist[:,0,0].size
-        self.nbDecision=intlist[0,0].size
-        
-    def arecompatible(self):
-        """Check whether the set of probability intervals are compatible, i.e., if the conjunction is non-empty.
-        
-        Return 1 if non-empty, 0 if empty
-        
-        >>> from numpy import array
-        >>> from classifip.representations.intervalsProbability import SetIntProba
-        >>> setproba=array([[[0.6,0.5,0.2],[0.4,0.3,0.]],[[0.55,0.55,0.2],[0.35,0.35,0.]]])
-        >>> setip.arecompatible()
-        1
-        >>> setproba=array([[[0.6,0.5,0.2],[0.4,0.3,0.]],[[0.55,0.55,0.2],[0.35,0.35,0.]],
-        ... [[0.5,0.2,0.6],[0.3,0.,0.4]],[[0.35,0.6,0.35],[0.15,0.4,0.15]]])
-        >>> setip=SetIntProba(setproba)
-        >>> setip.arecompatible()
-        0
-        """
-        comp=1
-        min=self.intlist[:,1,:].max(axis=0)
-        max=self.intlist[:,0,:].min(axis=0)
-        if min.sum() >= 0.9999999 or max.sum() <= 0.9999999:
-            comp=0
+    def __and__(self,other):
+        """Compute the intersection of two probability intervals
+        """  
+        mini=np.maximum(self.lproba[1,:],other.lproba[1,:])
+        maxi=np.minimum(self.lproba[0,:],other.lproba[0,:])
+        if mini.sum() >= 0.9999999 or maxi.sum() <= 0.9999999:
+            raise Exception('empty intersection')
         for i in range(self.nbDecision):
-            if min[i] >= max[i] - 0.0000001:
-                comp=0
-        return comp
-            
-    def conjunction(self):
-        """Perform a conjunctive merging of the set of probability intervals
-        
-        Return a possibly non-proper IntervalsProbability class object.
-        
-        >>> from numpy import array
-        >>> from classifip.representations.intervalsProbability import SetIntProba
-        >>> setproba=array([[[0.6,0.5,0.2],[0.4,0.3,0.]],[[0.55,0.55,0.2],[0.35,0.35,0.]]])
-        >>> setip=SetIntProba(setproba)
-        >>> print(setip.conjunction())
-                         y0    y1    y2 
-                   --------------------
-        upper bound | 0.550 0.500 0.200
-        lower bound | 0.400 0.350 0.000
-
-        """
-        if self.arecompatible() == 0:
-            raise Exception('Probability intervals not compatible, conjunction empty') 
+            if mini[i] >= maxi[i] - 0.0000001:
+                raise Exception('empty intersection')
         fusedproba=np.zeros((2,self.nbDecision))
-        for i in range(self.nbDecision):
-            subset=np.ones(self.nbDecision)
-            subset[i]=0
-            lb=max(self.intlist[:,1,i].max(),1-self.intlist[:,0,subset[:]==1].min(axis=0).sum())
-            ub=min(self.intlist[:,0,i].min(),1-self.intlist[:,1,subset[:]==1].max(axis=0).sum())
-            fusedproba[1,i]=lb
-            fusedproba[0,i]=ub
+        fusedproba[1,:]=mini
+        fusedproba[0,:]=maxi
         result=IntervalsProbability(fusedproba)
+        result.setreachableprobability()
         return result
-        
-    def disjunction(self):
-        """Perform a disjunctive merging of the set of probability intervals
-        
-        Return an IntervalsProbability class object.
-        
-        >>> from numpy import array
-        >>> from classifip.representations.intervalsProbability import SetIntProba
-        >>> setproba=array([[[0.6,0.5,0.2],[0.4,0.3,0.]],[[0.55,0.55,0.2],[0.35,0.35,0.]],
-        ... [[0.5,0.2,0.6],[0.3,0.,0.4]],[[0.35,0.6,0.35],[0.15,0.4,0.15]]])
-        >>> setip=SetIntProba(setproba)
-        >>> print(setip.disjunction())
-                         y0    y1    y2 
-                   --------------------
-        upper bound | 0.600 0.600 0.600
-        lower bound | 0.150 0.000 0.000
-
-        """
+    
+    def __or__(self,other):
+        """Compute the union of two probability intervals
+        """  
         fusedproba=np.zeros((2,self.nbDecision))
-        for i in range(self.nbDecision):
-            subset=np.ones(self.nbDecision)
-            subset[i]=0
-            lb=self.intlist[:,1,i].min()
-            ub=self.intlist[:,0,i].max()
-            fusedproba[1,i]=lb
-            fusedproba[0,i]=ub
+        fusedproba[1,:]=np.minimum(self.lproba[1,:],other.lproba[1,:])
+        fusedproba[0,:]=np.maximum(self.lproba[0,:],other.lproba[0,:])
         result=IntervalsProbability(fusedproba)
         return result
     
-    def average(self):
-        """Perform an average (equal weights) of the set of probability intervals
-        
-        Return an IntervalsProbability class object.
-        
-        >>> from numpy import array
-        >>> from classifip.representations.intervalsProbability import SetIntProba
-        >>> setproba=array([[[0.6,0.5,0.2],[0.4,0.3,0.]],[[0.55,0.55,0.2],[0.35,0.35,0.]],
-        ... [[0.5,0.2,0.6],[0.3,0.,0.4]],[[0.35,0.6,0.35],[0.15,0.4,0.15]]])
-        >>> setip=SetIntProba(setproba)
-        >>> print(setip.average())
-                         y0    y1    y2 
-                   --------------------
-        upper bound | 0.500 0.463 0.338
-        lower bound | 0.300 0.262 0.138
-
-
-        """
+    def __add__(self,other):
+        """Compute the average of two probability intervals
+        """  
         fusedproba=np.zeros((2,self.nbDecision))
-        for i in range(self.nbDecision):
-            subset=np.ones(self.nbDecision)
-            subset[i]=0
-            lb=self.intlist[:,1,i].sum()/self.nbProbInt
-            ub=self.intlist[:,0,i].sum()/self.nbProbInt
-            fusedproba[1,i]=lb
-            fusedproba[0,i]=ub
+        fusedproba[1,:]=np.mean([self.lproba[1,:],other.lproba[1,:]],axis=0)
+        fusedproba[0,:]=np.mean([self.lproba[0,:],other.lproba[0,:]],axis=0)
         result=IntervalsProbability(fusedproba)
         return result
-
-
