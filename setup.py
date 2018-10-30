310247727df7827b3764ae6d8fcd25b8de0bd34e
Yonatan C. Carranza Alarcon <salmuz@gmail.com>
2018-10-30 14:14:23 +0100

    new setup for install

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Intended Audience :: Science/Research
 Topic :: Scientific/Engineering :: Artificial Intelligence
 Topic :: Scientific/Engineering :: Mathematics
 Programming Language :: Python
-Programming Language :: Python :: 2
+Programming Language :: Python :: 3
 Operating System :: OS Independent"""
 
 setup(
@@ -18,12 +18,12 @@ setup(
     author='Sebastien Destercke',
     author_email='sebastien.destercke@hds.utc.fr',
     packages=find_packages(),
-    #['Classifip','Classifip.dataset', 'Classifip.evaluation', 'Classifip.models', 'Classifip.representations'],
     url='http://pypi.python.org/pypi/classifip/',
     license="GNU General Public License (GPL)",
     platforms = "any",
     description='Classification with Imprecise Probability methods.',
     long_description=open('README.rst').read(),
-    classifiers = classifiers.split('\n'), install_requires=['numpy', 'cvxopt', 'scikit-learn', 'matplotlib', 'pandas',
-                                                             'feather-format']
+    classifiers = classifiers.split('\n'),
+    install_requires=['numpy', 'cvxopt', 'scikit-learn', 'matplotlib', 'pandas',
+                      'Orange3', 'python-constraint', 'feather-format']
 )
