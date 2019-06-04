6542b3a2d332da6c7e6d2ddf74b0c48e0bd590a2
Yonatan C. Carranza Alarcon <salmuz@gmail.com>
2019-06-04 10:57:44 +0200

    Chagement de method inference de testing + data for testing

--- examples/ordinalregression/LWRaking_CSP.py
+++ examples/ordinalregression/LWRaking_CSP.py
@@ -6,7 +6,7 @@ print("Example of Ordinal regression learning - data set LEV \n")
 # model = classifip.models.nccof.NCCOF()
 model = classifip.models.ncclr.NCCLR()
 dataArff= classifip.dataset.arff.ArffFile()
-dataArff.load("/Users/salmuz/Downloads/datasets/iris_dense.xarff")
+dataArff.load("iris_dense.xarff")
 dataArff.discretize(discmet="eqfreq", numint=5)
 
 # Learning
@@ -33,6 +33,6 @@ for clazz, classifier in test[0].items():
     print(classifier.getmaximaldecision(ranking_utility))
 
 print("\nConstraint Satisfy Problem for making inference(reduction/propagation)")
-inference_csp = classifip.models.ncclr.inference_ranking_csp(test)
+inference_csp = model.predict_CSP(test)
 if inference_csp is None : print("\nWithout response CSP ranking.")
 else: print("\nCSP ranking response:", inference_csp)
