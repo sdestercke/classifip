304b179b2c9a8b051235e5e41a25addf9b55796e
Yonatan Carranza <salmuz@gmail.com>
2019-12-10 03:20:07 +0100

    saving a propre trace of acc. optimal ells

--- experiments/classification/qda/qda_multitask_cv.py
+++ experiments/classification/qda/qda_multitask_cv.py
@@ -1,15 +1,14 @@
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
-from classifip.evaluation.measures import u65, u80
 from classifip.utils import create_logger, normalize_minmax
 import sys, random, os, csv, numpy as np, pandas as pd
-from qda_common import __factory_model, generate_seeds
+from qda_common import generate_seeds
 from qda_manager import computing_training_testing_step, ManagerWorkers
 
-## Server env:
-# export LD_PRELOAD=/usr/local/MATLAB/R2018b/sys/os/glnxa64/libstdc++.so.6.0.22
-QPBB_PATH_SERVER = ['/home/lab/ycarranz/QuadProgBB', '/opt/cplex128/cplex/matlab/x86-64_linux']
 
+# Server env:
+# export LD_PRELOAD=/usr/local/MATLAB/R2018b/sys/os/glnxa64/libstdc++.so.6.0.22
+# QPBB_PATH_SERVER = ['/home/lab/ycarranz/QuadProgBB', '/opt/cplex128/cplex/matlab/x86-64_linux']
+# QPBB_PATH_SERVER = ['/volper/users/ycarranz/QuadProgBB', '/volper/users/ycarranz/cplex128/cplex/matlab/x86-64_linux']
 
 def computing_best_imprecise_mean(in_path=None, out_path=None, lib_path_server=None, model_type="ilda",
                                   from_ell=0.1, to_ell=1.0, by_ell=0.1, seed=None, cv_kfold_first=10,
@@ -49,6 +48,10 @@ def computing_best_imprecise_mean(in_path=None, out_path=None, lib_path_server=N
         logger.info("Splits %s learning %s", idx_kfold, idx_learning)
         logger.info("Splits %s testing %s", idx_kfold, idx_testing)
 
+        # # n-Skipping sampling and reboot parameter from_ell to 0.01 next sampling
+        if skip_nfold != 0 and idx_kfold > skip_nfold:
+            from_ell = 0.01
+
         # n-Skipping fold cross-validation (purpose for parallel computing)
         if idx_kfold >= skip_nfold:
             # Generate same k-fold-second (train, test) for impartially computing accuracy all ell parameters
@@ -82,29 +85,33 @@ def computing_best_imprecise_mean(in_path=None, out_path=None, lib_path_server=N
             logger.debug("Total-ell-k-step (%s, %s, %s, %s)", in_path, idx_kfold, ell_u65, ell_u80)
 
             # Computing optimal ells for using in testing step
-            acc_ellu80 = max(ell_u80.values())
-            acc_ellu65 = max(ell_u65.values())
-            ellu80_opts = [k for k, v in ell_u80.items() if v == acc_ellu80]
-            ellu65_opts = [k for k, v in ell_u65.items() if v == acc_ellu65]
+            acc_ell_u80 = max(ell_u80.values())
+            acc_ell_u65 = max(ell_u65.values())
+            ell_u80_opts = [k for k, v in ell_u80.items() if v == acc_ell_u80]
+            ell_u65_opts = [k for k, v in ell_u65.items() if v == acc_ell_u65]
             acc_u65[idx_kfold], acc_u80[idx_kfold] = 0, 0
-            n_ell80_opts, n_ell65_opts = len(ellu80_opts), len(ellu65_opts)
-            for ellu80_opt in ellu80_opts:
-                logger.info("ELL_OPTIMAL_CV_U80 %s", ellu80_opt)
-                _, acc_u80[idx_kfold] = \
-                    computing_training_testing_step(X_learning, y_learning, X_testing, y_testing, ellu80_opt,
-                                                    manager, 0, acc_u80[idx_kfold])
+            n_ell80_opts, n_ell65_opts = len(ell_u80_opts), len(ell_u65_opts)
+            for ell_u80_opt in ell_u80_opts:
+                logger.info("ELL_OPTIMAL_CV_U80 %s", ell_u80_opt)
+                _, _acc_u80 = \
+                    computing_training_testing_step(X_learning, y_learning, X_testing, y_testing, ell_u80_opt,
+                                                    manager, 0, 0)
+                acc_u80[idx_kfold] += _acc_u80
+                writer.writerow([-999, -8, ell_u80_opt, _acc_u80])
 
-            for ellu65_opt in ellu65_opts:
-                logger.info("ELL_OPTIMAL_CV_U65 %s", ellu65_opt)
-                acc_u65[idx_kfold], _ = \
-                    computing_training_testing_step(X_learning, y_learning, X_testing, y_testing, ellu65_opt,
-                                                    manager, acc_u65[idx_kfold], 0)
+            for ell_u65_opt in ell_u65_opts:
+                logger.info("ELL_OPTIMAL_CV_U65 %s", ell_u65_opt)
+                _acc_u65, _ = \
+                    computing_training_testing_step(X_learning, y_learning, X_testing, y_testing, ell_u65_opt,
+                                                    manager, 0, 0)
+                acc_u65[idx_kfold] += _acc_u65
+                writer.writerow([-999, -7, ell_u65_opt, _acc_u65])
 
             acc_u65[idx_kfold] = acc_u65[idx_kfold] / n_ell65_opts
             acc_u80[idx_kfold] = acc_u80[idx_kfold] / n_ell80_opts
             writer.writerow([-999, idx_kfold, acc_u65[idx_kfold], acc_u80[idx_kfold]])
             file_csv.flush()
-            logger.debug("Partial-ell-2step (u80, u65, accs) (%s, %s, %s, %s, %s)", -999, ellu80_opts, ellu65_opts,
+            logger.debug("Partial-ell-2step (u80, u65, accs) (%s, %s, %s, %s, %s)", -999, ell_u80_opts, ell_u65_opts,
                          acc_u65[idx_kfold], acc_u80[idx_kfold])
         idx_kfold += 1
     writer.writerow([-9999, -9, np.mean(list(acc_u65.values())), np.mean(list(acc_u80.values()))])
