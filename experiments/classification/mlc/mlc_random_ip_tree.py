83a5f151841719d4ccc55ec05052ba16d61021d1
Yonatan C. Carranza Alarcon <salmuz@gmail.com>
2020-03-07 20:14:11 +0100

    First improving for more 16 labels with exact algorithm skeptic multilabel

--- experiments/classification/mlc/mlc_random_ip_tree.py
+++ experiments/classification/mlc/mlc_random_ip_tree.py
@@ -78,6 +78,56 @@ def getlowerexpectation(cost_vector, root):
     return lowerExp(root)
 
 
+def getlowerexpectationGeneration(root):
+    import xxhash
+    leafs = dict()
+    leafs_var = dict()
+
+    def __hash(query):
+        _hash = xxhash.xxh64()
+        _hash.update(query)
+        res_hash = _hash.hexdigest()
+        _hash.reset()
+        return res_hash
+
+    def getlowerexpectation(prob, is_leaf=False, idx0=None, idx1=None):
+        if is_leaf:
+            expDichStr = "min(c[%s]*%s+c[%s]*%s,c[%s]*%s+c[%s]*%s)" % \
+                         (idx0, prob[1, 0], idx1, prob[0, 1], idx0, prob[0, 0], idx1, prob[1, 1])
+            name_var = "a" + str(idx0) + str(idx1)
+            leafs_var[__hash(expDichStr)] = name_var + "=" + expDichStr
+            leafs[__hash(expDichStr)] = name_var
+        else:
+            keyx0 = __hash(idx0)
+            new_idx0 = leafs[keyx0] if keyx0 in leafs else idx0
+            keyx1 = __hash(idx1)
+            new_idx1 = leafs[keyx1] if keyx1 in leafs else idx1
+            expDichStr = "min(%s*%s+%s*%s,%s*%s+%s*%s)" \
+                         % (new_idx0, prob[1, 0], new_idx1, prob[0, 1], new_idx0, prob[0, 0], new_idx1, prob[1, 1])
+            name_var = str(new_idx0) + str(new_idx1)
+            leafs_var[__hash(expDichStr)] = name_var + "=" + expDichStr
+            leafs[__hash(expDichStr)] = name_var
+
+        return expDichStr
+
+    def lowerExp(node):
+        bound_probabilities = node["prob"]
+        if "prob" not in node["0"] or "prob" not in node["1"]:
+            expInfStr = getlowerexpectation(bound_probabilities.lproba,
+                                            is_leaf=True,
+                                            idx0=binaryToDecimal(node["0"]["label"]),
+                                            idx1=binaryToDecimal(node["1"]["label"]))
+        else:
+            expInfStr = getlowerexpectation(bound_probabilities.lproba,
+                                            idx0=lowerExp(node["0"]),
+                                            idx1=lowerExp(node["1"]))
+        return expInfStr
+
+    lexp = lowerExp(root)
+    lexp_cost_var = "\n\t".join(list(leafs_var.values())[:-1])
+    return lexp, lexp_cost_var
+
+
 def marginal_probabilities_v1(root, nb_labels):
     all_output_space = np.array(list(product([0, 1], repeat=nb_labels)))
     resulting_score = np.zeros((nb_labels, 2))
@@ -94,14 +144,33 @@ def inference_exact_inference(root, nb_labels):
     indices_labels = list(range(nb_labels))
     all_output_space = list(product([0, 1], repeat=nb_labels))
     maximality_sets = dict.fromkeys(all_output_space, True)
+    cardinal_all_output = len(all_output_space)
+    all_output_space = np.array(all_output_space)
+
     PARTIAL_VALUE = 3
+    expectation_infimum_str, exp_cost_leaf_var = getlowerexpectationGeneration(root)
+    # print("global expectation\ndef expectation(c):\n\t" + exp_cost_leaf_var + "\n\treturn " + expectation_infimum_str)
+    exec("global exec_expectation_inf\n"
+         "def exec_expectation_inf(c):\n\t" + exp_cost_leaf_var + "\n\treturn " + expectation_infimum_str)
 
     def calculation_cost(a_sub_vector, neq_idx_labels=None):
-        _cost_vector = []
-        for y in all_output_space:
-            y_sub_array = np.array(y)[neq_idx_labels] if neq_idx_labels is not None else y
-            _cost_vector.append(hamming_distance(y_sub_array, a_sub_vector))
-        return np.array(_cost_vector)
+        """
+            Applying by column with a binary singleton Hamming loss function:
+                        L(y1, y2) = 1_{y1 \neq y2}
+                        L(y1, y2) = 1_{y1 ==0 and y2==1} + 1_{y1 ==1 and y2==0}
+                        L(y1, y2) = (1-y1)*y2 + (1-y2)*y1
+                        L(y1, y2) = y1 + y2 - 2*y2*y1
+                        L(y1, y2) = (y2 - y1)^2 = | y2 - y1 |
+            This operation can be computed by column to get vector cost hamming multilabel
+        :param a_sub_vector:
+        :param neq_idx_labels:
+        :return:
+        """
+        a_fully = np.array([a_sub_vector, ] * cardinal_all_output)
+        if neq_idx_labels is not None:
+            return np.sum(abs(all_output_space[:, neq_idx_labels] - a_fully), axis=1)
+        else:
+            return np.sum(abs(all_output_space - a_fully), axis=1)
 
     def expansion_partial_binary_vector(binary_vector):
         new_partial_vector = list()
@@ -134,16 +203,16 @@ def inference_exact_inference(root, nb_labels):
         for dominated in set_dominated_preds:
             maximality_sets[dominated] = False
 
-    def inf_not_equal_labels(root, nb_labels, p_n_not_equal_indices, p_neq_idx=None):
+    def inf_not_equal_labels(nb_labels, p_n_not_equal_indices, p_neq_idx=None):
         for a_vector in product([0, 1], repeat=p_n_not_equal_indices):
             partial_prediction = PARTIAL_VALUE * np.ones(nb_labels, dtype=int)
             partial_prediction[p_neq_idx] = np.array(a_vector)
             is_not_dominated, set_dominated_preds = is_solution_not_dominated(partial_prediction)
-            # print("%s ==> is_dominated %s" % (partial_prediction, is_not_dominated), flush=True)
+            # print("%s ==> is_not_dominated %s" % (partial_prediction, is_not_dominated), flush=True)
             if is_not_dominated:
                 not_a_vector = 1 - np.array(a_vector)
                 cost_vector = calculation_cost(not_a_vector, p_neq_idx)
-                inf_expectation = getlowerexpectation(cost_vector, root)
+                inf_expectation = exec_expectation_inf(cost_vector)  # getlowerexpectationV2(cost_vector, root)
                 # print("%s >_M %s ==> %s <? %s" % (
                 #     partial_prediction, not_a_vector, (p_n_not_equal_indices * 0.5),
                 #     inf_expectation), flush=True)
@@ -155,9 +224,9 @@ def inference_exact_inference(root, nb_labels):
         n_not_equal_indices = (nb_labels - 1) - i
         a_set_indices = list(combinations(indices_labels, n_not_equal_indices))
         for neq_idx in a_set_indices:
-            inf_not_equal_labels(root, nb_labels, n_not_equal_indices, list(neq_idx))
+            inf_not_equal_labels(nb_labels, n_not_equal_indices, list(neq_idx))
     # none equal labels (all different)
-    inf_not_equal_labels(root, nb_labels, nb_labels)
+    inf_not_equal_labels(nb_labels, nb_labels)
 
     solution_exact = list(filter(lambda k: maximality_sets[k], maximality_sets.keys()))
     # _logger.debug("set solutions improved exact inference %s", solution_exact)
@@ -179,16 +248,19 @@ def parallel_inferences(pid_tree, nb_labels, epsilon):
 
 
 def computing_outer_vs_exact_inference_random_tree(out_path,
-                                                   step_ncc_s=0.1,
                                                    nb_labels=3,
                                                    nb_repeats=100,
                                                    nb_process=1,
-                                                   seed=None):
+                                                   seed=None,
+                                                   min_epsilon_param=0.05,
+                                                   max_epsilon_param=0.5,
+                                                   step_epsilon_param=0.1):
     assert os.path.exists(out_path), "File for putting results does not exist"
 
     logger = create_logger("computing_outer_vs_exact_inference_random_tree", True)
     if seed is not None:
-        random.seed(seed)
+        seed = random.randrange(pow(2, 20))
+    random.seed(seed)
     logger.debug("[FIRST-STEP-SEED] SEED: %s", seed)
 
     # Create a CSV file for saving results
@@ -196,7 +268,7 @@ def computing_outer_vs_exact_inference_random_tree(out_path,
     writer = csv.writer(file_csv)
 
     POOL = multiprocessing.Pool(processes=nb_process)
-    for epsilon in np.arange(0.05, 0.5, step_ncc_s):
+    for epsilon in np.arange(min_epsilon_param, max_epsilon_param, step_epsilon_param):
         target_function = partial(parallel_inferences, nb_labels=nb_labels, epsilon=epsilon)
         set_distance_cardinal = POOL.map(target_function, range(nb_repeats))
         writer.writerow(np.hstack((epsilon, set_distance_cardinal)))
@@ -210,5 +282,8 @@ sys_nb_labels = 4
 sys_out_path = "results_labels" + str(sys_nb_labels) + ".csv"
 computing_outer_vs_exact_inference_random_tree(out_path=sys_out_path,
                                                nb_labels=sys_nb_labels,
-                                               nb_repeats=10000,
-                                               nb_process=1)
+                                               nb_repeats=10,
+                                               nb_process=1,
+                                               min_epsilon_param=0.5,
+                                               max_epsilon_param=6.0,
+                                               step_epsilon_param=1.0)
