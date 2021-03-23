389e390fb40ce33f6761511d030fed30f38b1e6a
Yonatan C. Carranza Alarcon <salmuz@gmail.com>
2021-03-23 06:55:06 +0100

    for name of features of text data set

--- classifip/dataset/arff.py
+++ classifip/dataset/arff.py
@@ -547,7 +547,7 @@ class ArffFile(object):
         self.relation = l[1]
 
     def __parse_attribute(self, l):
-        p = re.compile(r'[a-zA-Z_-][a-zA-Z0-9_-]*|\{[^\}]+\}|\'[^\']+\'|\"[^\"]+\"')
+        p = re.compile(r'[a-zA-Z0-9_-][a-zA-Z0-9_-]*|\{[^\}]+\}|\'[^\']+\'|\"[^\"]+\"')
         l = [s.strip() for s in p.findall(l)]
         name = l[1].replace("'","") # Modification : all ' in typename are removed
         atype = l[2]
